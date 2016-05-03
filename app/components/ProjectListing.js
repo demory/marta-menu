4998e30cda774c0341e6b1f7798d295577777239
David Emory <demory@conveyal.com>
2016-05-03 11:59:39 -0400

    Project list styling

--- app/components/ProjectListing.js
+++ app/components/ProjectListing.js
@@ -18,14 +18,27 @@ export default class ProjectListing extends Component {
     const mainStyle = this.props.inverse
       ? {
           color: '#444',
-          fontFamily: "Lato"
+          fontFamily: "Lato",
         }
-      : {}
+      : {
+          marginBottom: '18px'
+        }
+
+    const headerStyle = {
+      marginBottom: '6px'
+    }
+
+    const switchStyle = {
+      float: 'left',
+      marginTop: '3px'
+    }
 
     const titleStyle = {
       fontSize: '20px',
+      lineHeight: '22px',
       fontWeight: 'bold',
-      marginBottom: '3px'
+      marginLeft: '52px',
+      marginTop: '-4px'
     }
 
     const descStyle = {
@@ -36,7 +49,7 @@ export default class ProjectListing extends Component {
     const costStyle = {
       float:'right',
       background: this.props.inverse ? '#ddd' : '#444',
-      padding: '3px 6px',
+      padding: '1px 5px',
       fontSize: '15px',
       fontWeight: 'bold',
       marginLeft: '4px'
@@ -45,15 +58,18 @@ export default class ProjectListing extends Component {
     const project = this.props.project
     return (
       <div style={mainStyle}>
-        <div style={titleStyle} >
+        <div style={headerStyle}>
           <Switch onChange={() => { this.props.projectToggled() }}
             checked={project.selected}
             checkedChildren={<Glyphicon glyph='ok' />}
             unCheckedChildren={<Glyphicon glyph='remove' />}
-            style={{ marginRight: '10px' }}
+            style={switchStyle}
           />
           <div style={costStyle}>{formatCost(getProjectCost(project))}</div>
-          {project.name}
+          <div style={titleStyle} >
+            {project.name}
+          </div>
+          <div style={{ clear: 'both' }}></div>
         </div>
         <div style={descStyle} >
           {project.description}
