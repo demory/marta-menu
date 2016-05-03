ec4f4caf2753d7a4061f111ef1506087799e6b4a
David Emory <demory@conveyal.com>
2016-05-03 19:01:05 -0400

    Update costing formula

--- app/util.js
+++ app/util.js
@@ -1,5 +1,21 @@
+import haversine from 'haversine'
+
 export function getProjectCost (project) {
-  return project.cost.capital + project.cost.operating * ATF_CONFIG.budget.years
+  if(project.cost && (project.cost.capital || project.cost.operating)) {
+    return project.cost.capital + project.cost.operating * ATF_CONFIG.budget.years
+  }
+  switch(project.category) {
+    case 'lrt':
+    case 'rail':
+    case 'rapid_bus':
+      const dist = getProjectLength(project.geojson.geometry.coordinates)
+      const modeCosts = ATF_CONFIG.budget[project.category]
+      const cap = dist * modeCosts.capital_cost_per_mile * modeCosts.local_capital_share
+      const ops = dist * modeCosts.operating_cost_per_mile * ATF_CONFIG.budget.years * modeCosts.local_operating_share
+      return cap + ops
+  }
+
+  return 0
 }
 
 export function formatCost (cost) {
@@ -8,4 +24,20 @@ export function formatCost (cost) {
   }
   return '$' + Math.round(cost/100000000)/10 + 'B'
 
+}
+
+function getProjectLength (coords) {
+  let dist = 0
+  for(let i = 0; i < coords.length - 1; i++) {
+    const start = {
+      longitude: coords[i][0],
+      latitude: coords[i][1]
+    }
+    const end = {
+      longitude: coords[i+1][0],
+      latitude: coords[i+1][1]
+    }
+    dist += haversine(start, end, {unit: 'mi'})
+  }
+  return dist
 }
